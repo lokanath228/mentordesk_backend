43e3911933a869f78282d7dfc907b2229b2d075d
lokanath m <loguvima228@gmail.com>
2022-02-18 10:54:54 +0530

    first commit

--- models/postMessage.js
+++ models/postMessage.js
@@ -1,23 +1,26 @@
-import mongoose from 'mongoose';
-// import { string } from 'prop-types';
+import mongoose from 'mongoose'
+// import { string } from 'prop-types'
 
 const postSchema = mongoose.Schema({
-    title:String,
-    message:String,
-    creator:String,
-    tags:[String],
-    selectedFile:String,
-    likeCount:{
-        type:Number,
-        default:0
-    },
-    createdAt:{
-        type:Date,
-        default:new Date()
-    },
+  title: String,
+  message: String,
+  creator: String,
+  fruits: String,
+  vegetable: String,
+  meat: String,
+  tags: [String],
 
-});
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+})
 
-const PostMessage = mongoose.model('PostMessage',postSchema);
+const PostMessage = mongoose.model('PostMessage', postSchema)
 
-export default PostMessage;
+export default PostMessage
